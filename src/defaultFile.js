6049ef6d2a784c9d074cb57ac79aa7b54cc1c471
Madorskii <madorskiy.017@yandex.ru>
2023-08-05 20:16:49 -0300

    project is ready

--- src/defaultFile.js
+++ src/defaultFile.js
@@ -1,1 +1,6 @@
+/* eslint-disable no-console */
+export default () => {
+  console.log('1 + 2');
+  return true;
+};
 console.log(1 + 2);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,1 @@
-/* eslint-disable no-console */
-export default () => {
-  console.log('1 + 2');
-  return true;
-};
 console.log(1 + 2);
