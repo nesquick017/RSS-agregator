7999e876d9eb746c7e763474cb89624f8d18a268
Madorskii <madorskiy.017@yandex.ru>
2023-08-06 16:08:19 -0300

    index.html and index.css moved to proper positions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/extensions */
-import './index.html';
-import './index.css';
+import '../index.html';
+import './stylesheets/index.css';
 import app from './addLink.js';
 
 app();
--- webpack.config.cjs
+++ webpack.config.cjs
@@ -21,7 +21,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src', 'index.html'),
+      template: path.resolve(__dirname, './index.html'),
     }),
     new MiniCssExtractPlugin({
       filename: '[name].[contenthash].css',
