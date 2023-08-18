45d55e02029e8b94300141142662ddafeff00333
Madorskii <madorskiy.017@yandex.ru>
2023-08-18 13:01:05 -0300

    update

--- src/watchers/watcher.js
+++ src/watchers/watcher.js
@@ -15,3 +15,21 @@ export default (initialState, callback) => {
   });
   return watchedData;
 };
+
+const getNewPosts = (state) => {
+  const promises = state.content.feeds.map(({ link, feedId }) =>
+    getAxiosResponse(link).then((response) => {
+      const { posts } = parser(response.data.contents);
+      const addedPosts = state.content.posts.map((post) => post.link);
+      const newPosts = posts.filter((post) => !addedPosts.includes(post.link));
+      if (newPosts.length > 0) {
+        createPosts(state, newPosts, feedId);
+      }
+      return Promise.resolve();
+    }),
+  );
+
+  Promise.allSettled(promises).finally(() => {
+    setTimeout(() => getNewPosts(state), timeout);
+  });
+};
